df8e9708488279a4183b6fad9510f352667d7402
times2049 <times21c@gmail.com>
2020-07-28 16:54:02 +0000

    first commit

---
+++ core/.ipynb_checkpoints/data_loader-checkpoint.py
@@ -0,0 +1,186 @@
+from pathlib import Path
+from itertools import chain
+import os
+import random
+
+from munch import Munch
+from PIL import Image
+import numpy as np
+
+import torch
+from torch.utils import data
+from torch.utils.data.sampler import WeightedRandomSampler
+from torchvision import transforms
+from torchvision.datasets import FashionMNIST
+
+
+def listdir(dname):
+    fnames = list(chain(*[list(Path(dname).rglob('*.' + ext))
+                          for ext in ['png', 'jpg', 'jpeg', 'JPG']]))
+    return fnames
+
+
+class DefaultDataset(data.Dataset):
+    def __init__(self, root, transform=None):
+        self.samples = listdir(root)
+        self.samples.sort()
+        self.transform = transform
+        self.targets = None
+
+    def __getitem__(self, index):
+        fname = self.samples[index]
+        img = Image.open(fname).convert('RGB')
+        if self.transform is not None:
+            img = self.transform(img)
+        return img
+
+    def __len__(self):
+        return len(self.samples)
+
+
+class ReferenceDataset(data.Dataset):
+    def __init__(self, dataset, transform=None):
+        self.samples = dataset.data
+        size = self.samples.size()
+        rndperm = torch.randperm(size[0])
+        self.samples2 = self.samples[rndperm] 
+        self.targets = dataset.targets
+        self.transform = transform
+ 
+    def __getitem__(self, index):
+        img = self.samples[index]
+        img2 = self.samples2[index]
+
+        if self.transform is not None:
+            img = self.transform(img)
+            img2 = self.transform(img2)
+        label = self.targets[index]
+    
+        return img, img2, label
+
+    def __len__(self):
+        return len(self.targets)
+
+
+def _make_balanced_sampler(labels):
+    class_counts = np.bincount(labels)
+    class_weights = 1. / class_counts
+    weights = class_weights[labels]
+    return WeightedRandomSampler(weights, len(weights))
+
+
+def get_train_loader(root, which='source', img_size=256,
+                     batch_size=8, prob=0.5, num_workers=4):
+    print('Preparing DataLoader to fetch %s images '
+          'during the training phase...' % which)
+
+    crop = transforms.RandomCrop(
+        img_size)
+    rand_crop = transforms.Lambda(
+        lambda x: crop(x) if random.random() < prob else x)
+
+    transform = transforms.Compose([
+                                    transforms.Grayscale(num_output_channels=3),
+        transforms.Pad(4),
+        transforms.RandomCrop(
+        img_size),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                             std=[0.5, 0.5, 0.5]),
+    ])
+    if which == 'source':
+        dataset = FashionMNIST("./data", train=True, transform=transform, 
+                               target_transform=None, download=True
+                              )
+    elif which == 'reference':
+        transform = transforms.Compose([transforms.ToPILImage(mode=None),
+                                    transforms.Grayscale(num_output_channels=3),
+        transforms.Pad(4),
+        transforms.RandomCrop(
+        img_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                                 std=[0.5, 0.5, 0.5]),
+        ])
+        dataset = ReferenceDataset(
+            FashionMNIST("./data", train=True, download=True),
+            transform=transform
+        )
+
+    else:
+        raise NotImplementedError
+
+    sampler = _make_balanced_sampler(dataset.targets)
+    return data.DataLoader(dataset=dataset,
+                           batch_size=batch_size,
+                           sampler=sampler,
+                           num_workers=num_workers,
+                           pin_memory=True,
+                           drop_last=True)
+
+
+
+def get_test_loader(root, img_size=256, batch_size=32,
+                    shuffle=True, num_workers=4):
+    print('Preparing DataLoader for the generation phase...')
+    transform = transforms.Compose([transforms.Grayscale(num_output_channels=3),
+        transforms.Pad(2),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                             std=[0.5, 0.5, 0.5]),
+    ])
+
+    dataset = FashionMNIST("./data", train=False, transform=transform, target_transform=None, download=True)
+    return data.DataLoader(dataset=dataset,
+                           batch_size=batch_size,
+                           shuffle=shuffle,
+                           num_workers=num_workers,
+                           pin_memory=True)
+
+
+class InputFetcher:
+    def __init__(self, loader, loader_ref=None, latent_dim=16, mode=''):
+        self.loader = loader
+        self.loader_ref = loader_ref
+        self.latent_dim = latent_dim
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.mode = mode
+
+    def _fetch_inputs(self):
+        try:
+            x, y = next(self.iter)
+        except (AttributeError, StopIteration):
+            self.iter = iter(self.loader)
+            x, y = next(self.iter)
+        return x, y
+
+    def _fetch_refs(self):
+        try:
+            x, x2, y = next(self.iter_ref)
+        except (AttributeError, StopIteration):
+            self.iter_ref = iter(self.loader_ref)
+            x, x2, y = next(self.iter_ref)
+        return x, x2, y
+
+    def __next__(self):
+        x, y = self._fetch_inputs()
+        if self.mode == 'train':
+            x_ref, x_ref2, y_ref = self._fetch_refs()
+            z_trg = torch.randn(x.size(0), self.latent_dim)
+            z_trg2 = torch.randn(x.size(0), self.latent_dim)
+            inputs = Munch(x_src=x, y_src=y, y_ref=y_ref,
+                           x_ref=x_ref, x_ref2=x_ref2,
+                           z_trg=z_trg, z_trg2=z_trg2)
+        elif self.mode == 'val':
+            x_ref, y_ref = self._fetch_inputs()
+            inputs = Munch(x_src=x, y_src=y,
+                           x_ref=x_ref, y_ref=y_ref)
+        elif self.mode == 'test':
+            inputs = Munch(x=x, y=y)
+        else:
+            raise NotImplementedError
+
+        return Munch({k: v.to(self.device)
+                      for k, v in inputs.items()})
